22a50a32f26665bd76d481d9500144fdd5a8bc3f
Marilaine Zorzan <mari_z7@hotmail.com>
2020-06-08 22:32:20 -0300

    Primeiro commit

---
+++ G/src/git/Git.java
@@ -0,0 +1,24 @@
+package git;
+
+import java.util.Scanner;
+
+public class Git {
+
+	public static void main(String[] args) {
+		
+		
+				Scanner scan = new Scanner(System.in);
+				
+				System.out.println("Digite seu nome:");
+				
+				String nome = scan.nextLine();
+				
+		        System.out.println("Digite sua idade:");
+				
+				int idade = scan.nextInt();
+				
+				System.out.println("Seu nome é : " + nome + "\n" + " Você tem " + idade + " anos");
+
+			}
+
+		}
